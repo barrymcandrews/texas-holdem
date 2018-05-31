c13af636a175055e63d96b7c9b7233d2d39faff0
Barry McAndrews <bmcandrews3@hotmail.com>
2018-05-31 10:43:53 -0400

    create swing skeleton

---
+++ src/main/java/GameController.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GameController implements Controller {
+    GamePanel gamePanel = new GamePanel();
+
+    public GameController() {
+        //TODO: Initialize game elements here
+    }
+
+    @Override
+    public JPanel getPanel() {
+        return gamePanel;
+    }
+}
