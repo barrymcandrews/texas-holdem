b983ec1fc7283cc62d4d550f288cefdd803b6d44
Laura Friedland <lnf14@pitt.edu>
2018-06-04 13:56:23 -0400

    Add error handling and handle cancel cases

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -26,12 +26,24 @@ public class Game {
         panel.add(new JLabel("Please enter the number of opponents:"));
         panel.add(playersField);
 
-        // get the results.
-        int result = JOptionPane.showConfirmDialog(null, panel, "Please enter the following:",
-                JOptionPane.OK_CANCEL_OPTION);
+        // get the results. if user presses cancel, exit
+        int result = JOptionPane.showConfirmDialog(null, panel, "Please enter the following:", JOptionPane.OK_CANCEL_OPTION);
         if (result == JOptionPane.OK_OPTION) {
             numberOfOpponents = (int) playersField.getSelectedItem();
             userName = nameField.getText();
+            // verify that name field isn't empty. numberOfOpponents will default to 1. Exit if cancel is selected
+            while (userName == null || userName.isEmpty()) {
+                JOptionPane.showMessageDialog(panel, "You did not enter your name. Please try again", "Error", JOptionPane.ERROR_MESSAGE);
+                int n = JOptionPane.showConfirmDialog(null, new JPanel().add(nameField), "Enter your name.", JOptionPane.OK_CANCEL_OPTION);
+                if (n == JOptionPane.CANCEL_OPTION)
+                    System.exit(0);
+                else
+                    userName = nameField.getText();
+            }
+        } else {
+            //exit if cancel is selected
+            System.out.println("Goodbye!");
+            System.exit(0);
         }
     }
 
