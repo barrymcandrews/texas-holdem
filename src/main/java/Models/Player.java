5c3aaebfc64ab05d805c8da2a6804eb455593d5d
cnm31 <cnm31@pitt.edu>
2018-06-04 20:33:10 -0400

    fixed my index boo boo, now we remove an AI from list if the user steals their name

--- src/main/java/Models/Player.java
+++ src/main/java/Models/Player.java
@@ -1,24 +1,35 @@
 package Models;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Player {
 
     private String name;
     private Role role;
     private int wallet;
+    private String[] AiNames = new String[] {"Bob", "Linda", "Tina", "Gene", "Louise", "Jimmy Jr.", "Teddy", "AndyenOllie"};
+    private ArrayList<String> AiNamesList = new ArrayList<>();
 
-    public Player(){this.wallet = 1000;}
+    public Player(){this.wallet = 1000; AiNamesList.addAll(Arrays.asList(AiNames));}
 
     public Player(String name, Role role){
         this.name = name;
         this.role = role;
         this.wallet = 1000;
+
+        AiNamesList.addAll(Arrays.asList(AiNames));
     }
 
-    public String[] getAiNames() {
-        return AiNames;
-    }
 
-    protected String[] AiNames = new String[] {"Bob", "Linda", "Tina", "Gene", "Louise", "Jimmy Jr.", "Teddy", "AndyenOllie"};
+    public void resetAiNames(String name){
+        for(int i = 0; i < AiNamesList.size(); i++){
+            if(AiNamesList.get(i).equals(name)){
+                AiNamesList.remove(name);
+            }
+        }
+    }
 
     public enum Role {
         PLAYER, DEALER
@@ -48,6 +59,14 @@ public class Player {
         this.wallet = wallet;
     }
 
+    public String[] getAiNames() {
+        return AiNames;
+    }
+
+
+    public ArrayList<String> getAiNamesList() {
+        return AiNamesList;
+    }
 
 
 }
