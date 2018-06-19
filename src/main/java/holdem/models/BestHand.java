ccbab39a5cb208e98f61dd296f9a7ee76ba3f724
Brendan Moorhead <bmm112@pitt.edu>
2018-06-18 21:06:33 -0400

    Added card equals

--- src/main/java/holdem/models/BestHand.java
+++ src/main/java/holdem/models/BestHand.java
@@ -1,5 +1,26 @@
 package holdem.models;
 
-public class BestHand
-{
+import java.util.HashSet;
+import java.util.Set;
+
+public class BestHand {
+
+    public static int findBestHand(Set<Card> playerHand, Set<Card> riverCards) {
+
+        Set<Card> cardsAvailable = new HashSet<>();
+
+        cardsAvailable.addAll(playerHand);
+        cardsAvailable.addAll(riverCards);
+
+        //check for straight flush
+
+        for(Card.Suit s : Card.Suit.values()) {
+            for(int i = 14; i > 1; i--) {
+                Card c = new Card(s, Card.intToValue(i));
+
+            }
+        }
+
+        return 7;
+    }
 }
