748561d60ef71fcdea03dcffa7a11bb2c9d46334
Laura Friedland <lnf14@pitt.edu>
2018-07-23 14:20:24 -0400

    Fix card layout

--- src/main/java/holdem/WinnerDialog.java
+++ src/main/java/holdem/WinnerDialog.java
@@ -1,5 +1,8 @@
 package holdem;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,18 +40,38 @@ public class WinnerDialog {
              }
         }
         
+//        JLabel nameLabel = new JLabel("Your Name:");
+//        c.anchor = LINE_END;
+//        c.gridx = 0;
+//        c.gridy = 1;
+//        c.gridwidth = 1;
+//        c.fill = GridBagConstraints.NONE;
+//        dialogPanel.add(nameLabel, c);
+        
+        dialog.setLayout(new GridBagLayout());
+        GridBagConstraints layout = new GridBagConstraints();
         for(Player p : winners) {
-           JLabel hand = new JLabel(hands.toString());
-           dialog.add(hand);
+           JLabel hand = new JLabel(hands.toString() + ":");
+           layout.anchor = GridBagConstraints.CENTER;
+           layout.gridx = 0;
+           layout.gridy = 0;
+           layout.fill = GridBagConstraints.NONE;
+           dialog.add(hand, layout);
            //TODO format cards correctly
            ArrayList<Card> cards = new ArrayList<>();
            cards.addAll(p.getHand());
            cards.addAll(Game.getInstance().getCenterCards());
            if(winningHand.getRank() != -1) {
+               layout.gridy = 1;
+               layout.fill = GridBagConstraints.NONE;
+               int x = 1;
                for(Integer v : winningHand.getCardValues()) 
                    for(Card c : cards) {
                        if(c.getIntValue() == v) {
-                           dialog.add(new JLabel(c.getImageIcon())); 
+                           JLabel label = new JLabel(resize(c.getImageIcon()));
+                           layout.gridx = x;
+                           dialog.add(label, layout);
+                           x++;
                        }
                    }
            }
@@ -57,5 +80,11 @@ public class WinnerDialog {
     
     public void show() {
         JOptionPane.showMessageDialog(null, dialog, names.toString(), JOptionPane.PLAIN_MESSAGE);
+    }
+    
+    private ImageIcon resize(ImageIcon img) {
+        Image image = img.getImage(); // transform it 
+        Image newimg = image.getScaledInstance(60, 90,  Image.SCALE_SMOOTH); // scale it the smooth way  
+        return new ImageIcon(newimg);  // transform it back
     }
 }
