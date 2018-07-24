a679ac412b9c7ae48e54d37f0bd29a0dddad433c
Laura Friedland <lnf14@pitt.edu>
2018-07-24 17:47:08 -0400

    Fix 6 cards displayed bug

--- src/main/java/holdem/WinnerDialog.java
+++ src/main/java/holdem/WinnerDialog.java
@@ -4,7 +4,9 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.swing.*;
 
@@ -15,10 +17,11 @@ import holdem.models.Player;
 public class WinnerDialog {
     private JPanel dialog = new JPanel();
     private StringBuilder names;
+    private StringBuilder hands;
     
     public WinnerDialog(List<Player> winners, HandScore winningHand) {
         names = new StringBuilder();
-        StringBuilder hands = new StringBuilder();
+        hands = new StringBuilder();
         String AND = " and ";
         String COMMA = ", ";
         if(winners.size() == 1) {
@@ -40,47 +43,79 @@ public class WinnerDialog {
              }
         }
         
-        dialog.setLayout(new GridBagLayout());
-        GridBagConstraints layout = new GridBagConstraints();
-        int y = 0;
-        for(Player p : winners) {
-           JLabel winExplanation = new JLabel(hands.toString() + ":");
-           layout.anchor = GridBagConstraints.CENTER;
-           layout.gridx = 0;
-           layout.gridy = y;
-           layout.fill = GridBagConstraints.NONE;
-           dialog.add(winExplanation, layout);
-           ArrayList<Card> cards = new ArrayList<>();
-           cards.addAll(p.getHand());
-           cards.addAll(Game.getInstance().getCenterCards());
-           ArrayList<Integer> hand = winningHand.getCardValues();
-           //if winning hand doesn't have 5 cards, add cards from all available cards until we have a hand of 5
-           if(hand.size() != 5) {
-               for(int i = hand.size(); i <= 5; i++) {
-                   if(!hand.contains(cards.get(i).getIntValue()))
-                           hand.add(cards.get(i).getIntValue());
-               }
-           }
-           if(winningHand.getRank() != -1) {
-               layout.gridy = y+1;
-               layout.fill = GridBagConstraints.NONE;
-               int x = 1;
-               for(Integer v : hand) 
-                   for(Card c : cards) {
-                       if(c.getIntValue() == v) {
-                           JLabel label = new JLabel(resize(c.getImageIcon()));
-                           layout.gridx = x;
-                           dialog.add(label, layout);
-                           x++;
-                       }
-                   }
-           }
-           y++;
-        }
+        setUpLayout(winners, winningHand);
+       
     }
     
     public void show() {
         JOptionPane.showMessageDialog(null, dialog, names.toString(), JOptionPane.PLAIN_MESSAGE);
+    }
+    
+    private void setUpLayout(List<Player> winners, HandScore winningHand) {
+        dialog.setLayout(new GridBagLayout());
+        GridBagConstraints layout = new GridBagConstraints();
+        int y = 0;
+        for (Player p : winners) {
+            // add win explantion to top of dialog
+            JLabel winExplanation = new JLabel(hands.toString() + ":");
+            layout.anchor = GridBagConstraints.CENTER;
+            layout.gridx = 0;
+            layout.gridy = y;
+            layout.fill = GridBagConstraints.NONE;
+            dialog.add(winExplanation, layout);
+
+            if (winningHand.getRank() != -1) {
+                ArrayList<Card> cards = getAllCards(p);
+                ArrayList<Integer> hand = getWinningHand(winningHand, cards);
+                Set<ImageIcon> cardImages = convertValueToCard(hand, cards);
+                layout.gridy = y++;
+                layout.fill = GridBagConstraints.NONE;
+                int x = 1;
+                while (x <= 5) {
+                    for (ImageIcon img : cardImages) {
+                        JLabel label = new JLabel(img);
+                        layout.gridx = x;
+                        dialog.add(label, layout);
+                        x++;
+                    }
+                }
+            }
+            y+=2;
+        }
+    }
+    
+    private ArrayList<Card> getAllCards(Player p) {
+        ArrayList<Card> cards = new ArrayList<>();
+        cards.addAll(p.getHand());
+        cards.addAll(Game.getInstance().getCenterCards());
+        return cards;
+    }
+    
+    private ArrayList<Integer> getWinningHand(HandScore winningHand, ArrayList<Card> cards) {
+        ArrayList<Integer> hand = winningHand.getCardValues();
+        //if winning hand doesn't have 5 cards, add cards from all available cards until we have a hand of 5
+        if(hand.size() != 5) {
+            for(int i = hand.size(); i <= 5; i++) {
+                if(!hand.contains(cards.get(i).getIntValue()))
+                        hand.add(cards.get(i).getIntValue());
+            }
+        }
+        return hand;
+    }
+    
+    private Set<ImageIcon> convertValueToCard(ArrayList<Integer> hand, ArrayList<Card> cards) {
+        // convert card value to card image and add all to dialog
+        Set<ImageIcon> images = new HashSet<>();
+        while(images.size() < 5) {
+            for (Integer v : hand) {
+                for (Card c : cards) {
+                    if (c.getIntValue() == v) 
+                        images.add(resize(c.getImageIcon()));
+                }
+            }
+        }
+        return images;
+
     }
     
     private ImageIcon resize(ImageIcon img) {
